688dabc5ac504fef36d112f1ed6c92f472572b5a
Dipesh Pokhrel <dipeshpokhrel45@gmail.com>
2022-08-06 14:10:54 +0545

    testcase for userapp

---
+++ user_app/tests.py
@@ -0,0 +1,42 @@
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+from rest_framework import status
+from rest_framework.test import APITestCase
+from rest_framework.authtoken.models import Token
+
+
+class RegisterTestCase(APITestCase):
+
+    def test_register(self):
+        data = {
+            'username': 'testuser',
+            'email': 'test@example.com',
+            'password': 'testpassword',
+            'password2': 'testpassword'
+        }
+        response = self.client.post(reverse('register'), data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+class LoginLogoutTestCase(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser',
+            password = 'testpassword'
+        )
+    
+    def test_login(self):
+        data = {
+            'username': 'testuser',
+            'password': 'testpassword'
+        }
+        response = self.client.post(reverse('login'), data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_logout(self):
+        self.token = Token.objects.get(user__username='testuser')
+        self.client.credentials(HTTP_AUTHORIZATION='Token'+ self.token.key)
+        response = self.client.post(reverse('logout'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
